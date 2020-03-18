c02aa21d4ff7996419966db0a59f8e9e5073fccf
yinhow <yinhowlew@gmail.com>
2020-03-18 18:38:01 -0400

    Updates

---
+++ precache-manifest.4d515944171b597a4117dfebaa05ed2f.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863fc9e8fbab633d1ae1bfd413adaf52",
+    "url": "/boxgreen-ecommerce-replica/index.html"
+  },
+  {
+    "revision": "0f5eb670e8913f27f81d",
+    "url": "/boxgreen-ecommerce-replica/static/css/2.385c8688.chunk.css"
+  },
+  {
+    "revision": "955a07d12553890c684b",
+    "url": "/boxgreen-ecommerce-replica/static/css/3.7451cc6a.chunk.css"
+  },
+  {
+    "revision": "df1069951fe1fd21a0bd",
+    "url": "/boxgreen-ecommerce-replica/static/css/4.3c015c7f.chunk.css"
+  },
+  {
+    "revision": "1e718e072f86d36d22a8",
+    "url": "/boxgreen-ecommerce-replica/static/css/5.6ecf7bb5.chunk.css"
+  },
+  {
+    "revision": "38bc67d7a32e839a79f9",
+    "url": "/boxgreen-ecommerce-replica/static/css/6.bc16d36f.chunk.css"
+  },
+  {
+    "revision": "aa882d395d746ae75cbc",
+    "url": "/boxgreen-ecommerce-replica/static/css/7.f00caf4a.chunk.css"
+  },
+  {
+    "revision": "7ba71276c376b29d85be",
+    "url": "/boxgreen-ecommerce-replica/static/css/8.2ce3e9ed.chunk.css"
+  },
+  {
+    "revision": "04836944bf24d84638ff",
+    "url": "/boxgreen-ecommerce-replica/static/css/main.d60374fd.chunk.css"
+  },
+  {
+    "revision": "0f5eb670e8913f27f81d",
+    "url": "/boxgreen-ecommerce-replica/static/js/2.50bc3a83.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/boxgreen-ecommerce-replica/static/js/2.50bc3a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955a07d12553890c684b",
+    "url": "/boxgreen-ecommerce-replica/static/js/3.268a0dc3.chunk.js"
+  },
+  {
+    "revision": "df1069951fe1fd21a0bd",
+    "url": "/boxgreen-ecommerce-replica/static/js/4.b8d2f331.chunk.js"
+  },
+  {
+    "revision": "1e718e072f86d36d22a8",
+    "url": "/boxgreen-ecommerce-replica/static/js/5.fa456273.chunk.js"
+  },
+  {
+    "revision": "38bc67d7a32e839a79f9",
+    "url": "/boxgreen-ecommerce-replica/static/js/6.b51ecd07.chunk.js"
+  },
+  {
+    "revision": "aa882d395d746ae75cbc",
+    "url": "/boxgreen-ecommerce-replica/static/js/7.a771d169.chunk.js"
+  },
+  {
+    "revision": "7ba71276c376b29d85be",
+    "url": "/boxgreen-ecommerce-replica/static/js/8.6dddcd44.chunk.js"
+  },
+  {
+    "revision": "04836944bf24d84638ff",
+    "url": "/boxgreen-ecommerce-replica/static/js/main.28c1cd6c.chunk.js"
+  },
+  {
+    "revision": "721487bb48433ac74545",
+    "url": "/boxgreen-ecommerce-replica/static/js/runtime-main.974b75cc.js"
+  },
+  {
+    "revision": "19a95730914b90cf523c0439bc5aa0e7",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Bold.19a95730.woff"
+  },
+  {
+    "revision": "20c817845a51e48798b4884a45a4dcbd",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Bold.20c81784.woff2"
+  },
+  {
+    "revision": "522ba9caf077edc39a28f7aba5a4cf46",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Bold.522ba9ca.eot"
+  },
+  {
+    "revision": "b426f8dcdafa8df731a378dc69b7557d",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Bold.b426f8dc.ttf"
+  },
+  {
+    "revision": "cb719d96a3a8e4d9888741a431d90249",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Bold.cb719d96.svg"
+  },
+  {
+    "revision": "3dd813544e22e4c942e9637bbb5e50cf",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Book.3dd81354.ttf"
+  },
+  {
+    "revision": "50d4a83c978f28faf36af6de3079cd4c",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Book.50d4a83c.eot"
+  },
+  {
+    "revision": "791f69e6cacc070aca8ebecdb8e0e3a2",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Book.791f69e6.woff"
+  },
+  {
+    "revision": "ade5999a38d5deff33631cd521f2d3dd",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Book.ade5999a.svg"
+  },
+  {
+    "revision": "aec68114beb2ca50ea4be4f9c598614c",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Book.aec68114.woff2"
+  },
+  {
+    "revision": "08b43ad9257c513d1ee664f7fc54ae36",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Light.08b43ad9.svg"
+  },
+  {
+    "revision": "0d5ad5635f8c3bbb4742e1ef9704dc38",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Light.0d5ad563.woff"
+  },
+  {
+    "revision": "1d64c0eef2ea9b2679dbae12259f3899",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Light.1d64c0ee.woff2"
+  },
+  {
+    "revision": "90e6ed5e2ba55bd0f2e6f2e8617b420a",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Light.90e6ed5e.eot"
+  },
+  {
+    "revision": "e536db50791a42bec808f071b87a06a6",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Light.e536db50.ttf"
+  },
+  {
+    "revision": "0ad47d056b98abe89db1e4be898e08c8",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Medium.0ad47d05.woff2"
+  },
+  {
+    "revision": "51a96cc8f2e31cdb9cea33ee8c7f1f7c",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Medium.51a96cc8.ttf"
+  },
+  {
+    "revision": "60179a5df8af340a538a8dcbe8ccaf26",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Medium.60179a5d.eot"
+  },
+  {
+    "revision": "ea152a7b020957be4781678c297c6ffd",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Medium.ea152a7b.svg"
+  },
+  {
+    "revision": "f369ff93a4078dcea7de5dbb4544d82b",
+    "url": "/boxgreen-ecommerce-replica/static/media/GothamRounded-Medium.f369ff93.woff"
+  },
+  {
+    "revision": "1aff14a3caa6294e58152ed55529eda2",
+    "url": "/boxgreen-ecommerce-replica/static/media/VarelaRound.1aff14a3.ttf"
+  },
+  {
+    "revision": "1c93e4e2d8b8d6306d60ab0cdf19aa63",
+    "url": "/boxgreen-ecommerce-replica/static/media/VarelaRound.1c93e4e2.woff"
+  },
+  {
+    "revision": "8131f6718b23b90b0155d588340f164a",
+    "url": "/boxgreen-ecommerce-replica/static/media/VarelaRound.8131f671.woff2"
+  },
+  {
+    "revision": "d997fa688dcdbcc153917428f074b6b2",
+    "url": "/boxgreen-ecommerce-replica/static/media/VarelaRound.d997fa68.svg"
+  },
+  {
+    "revision": "f7ffa6a4b638d3ecbcec6a1a3fef7184",
+    "url": "/boxgreen-ecommerce-replica/static/media/VarelaRound.f7ffa6a4.eot"
+  }
+]);
