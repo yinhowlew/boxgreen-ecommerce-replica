4bb557f6de0daf4e109f59e7de0ff79d75769c08
yinhow <yinhowlew@gmail.com>
2020-03-18 22:36:49 -0400

    fixed landing UI and sort UI

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './errorpage.css';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
 
@@ -9,7 +10,7 @@ const ErrorPage = () => {
 			<h2>Oops...something happened!</h2>
 			<h3> The page you were looking for doesn't exist.</h3>
 			<h3> You may have mistyped the address or the page may have moved </h3>
-			<a className="btn btn-primary btn-lg" href={`${process.env.PUBLIC_URL}/shop`}>start adding snacks</a>
+			<Link className="btn btn-primary btn-lg" to="/shop">start adding snacks</Link>
 		</div>	
 	);
 }
